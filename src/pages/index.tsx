0f56effcf2a148e6751400027723eee703db78f2
Martín M <skydiver@users.noreply.github.com>
2022-10-25 20:26:06 -0300

    updated home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
-const Home = () => <div>Hello World!</div>;
+const Home = () => (
+  <div className="w-screen h-screen px-8 flex items-center justify-center font-semibold text-3xl text-center">
+    Next.js starter template with TypeScript + Tailwind CSS
+  </div>
+);
 
 export default Home;
